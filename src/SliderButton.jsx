270b3ee82f55eccc450d78121c54ed942e89a326
Marzieh Valizadeh <marziehvalizadeh7@gmail.com>
2023-11-13 12:08:19 +0330

    feat: add embla slider

---
+++ src/SliderButton.jsx
@@ -0,0 +1,12 @@
+import nexIcon from "./assets/prev.svg"
+
+export const PrevButton = ({ enabled, onClick ,style}) => (
+    <button  onClick={onClick} disabled={!enabled} className="embla__button">
+      <img alt="" src={nexIcon} style={{transform: "rotate(180deg)", ...style}} />
+    </button>
+  )
+export const NextButton = ({ enabled, onClick ,style}) => (
+    <button  onClick={onClick} disabled={!enabled} className="embla__button">
+      <img alt="" src={nexIcon} style={style} />
+    </button>
+  )
---
+++ src/components/slider.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import { PrevButton, NextButton } from '../SliderButton'
+
+export const EmblaCarousel = ({sliders }) => {
+  const [emblaRef,embla] = useEmblaCarousel({ loop: true })
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        {sliders?.map(item =>{
+          return <div className="embla__slide" key={item?.id}>
+            <div className="embla__slide__inner">
+                <img
+                  className="embla__slide__img"
+                  src={item?.imageUrl}
+                  alt=''
+                />
+              </div>
+          </div>
+        })}
+      </div>
+      <div className='embla__button__container'>
+        <PrevButton onClick={()=> embla?.scrollPrev(true)} enabled style={{width: "20px"}} />
+        <NextButton onClick={()=> embla?.scrollNext(true)} enabled style={{width: "20px"}} />
+      </div>
+    </div>
+  )
+}
